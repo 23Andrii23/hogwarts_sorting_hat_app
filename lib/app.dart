5e9c017a7c982d4ac11c2a74aee04b8e4671bcd4
Andrii Prystaiko <prystaiko@C02V40Y0HTD5.local>
2025-01-30 19:38:06 +0200

    feat: add app icon and splash screen

--- lib/app.dart
+++ lib/app.dart
@@ -1,17 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:hogwarts/screens/main_page/main_page.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-void main() {
-  runApp(
-    const ProviderScope(
-      child: MyApp(),
-    ),
-  );
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class App extends StatelessWidget {
+  const App({super.key});
 
   @override
   Widget build(BuildContext context) {
